e4bffb19a5531655613eb14ec7e861d52e0709ef
Xiaofeng Wang <henrywangxf@me.com>
2021-03-31 22:31:02 +0800

    stage: Add aws parallel job

---
+++ jobs-stage/cki/job_cki_parallel_aws_rhel8.groovy
@@ -0,0 +1,32 @@
+// Folder name and description
+String folderName = 'cki'
+String folderDescription = 'cki for cloud'
+// Repository URL
+String repo = 'https://github.com/virt-s1/cki.git'
+
+folder(folderName) {
+    description(folderDescription)
+}
+
+pipelineJob("$folderName/rhel-8.5-aws") {
+    description("run rhel-8.5 stream kernel test on aws")
+    logRotator(-1, 20, -1, -1)
+    parameters {
+        stringParam('CI_MESSAGE', '{}', 'Red Hat UMB Message Body')
+        stringParam('TEST_OS', 'rhel-8-5', 'test os')
+    }
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url(repo)
+                    }
+                    branch('*/parallel')
+                }
+            }
+            lightweight(true)
+            scriptPath("cloudci/Jenkinsfile.parallel.aws")
+        }
+    }
+}
