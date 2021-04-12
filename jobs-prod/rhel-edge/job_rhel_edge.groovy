cda9dc37481a4ec09d980274331b92b2404f0b64
Xiaofeng Wang <henrywangxf@me.com>
2021-04-12 13:14:08 +0800

    prod: Move rhel-edge jobs to prod

--- jobs-prod/rhel-edge/job_rhel_edge.groovy
+++ jobs-prod/rhel-edge/job_rhel_edge.groovy
@@ -1,9 +1,9 @@
 // Folder name and description
 String folderName = 'rhel-edge'
-String folderDescription = 'CI for rhel 8.4.0 edge image building and installation'
+String folderDescription = 'CI for rhel 8 edge image building and installation'
 // Job name and description
-String jobName = 'rhel840_edge_bare_x86_64'
-String jobDescription = 'nightly x86_64 compose triggers rhel 8.4.0 edge test'
+String jobName = 'rhel_edge_x86_64'
+String jobDescription = 'nightly rhel compose triggers rhel for edge test'
 // Repository URL
 String repo = 'https://github.com/virt-s1/rhel-edge.git'
 
@@ -16,8 +16,6 @@ pipelineJob("$folderName/$jobName") {
     logRotator(-1, 20, -1, -1)
     parameters {
         stringParam('CI_MESSAGE', '{}', 'Red Hat UMB Message Body')
-        stringParam('TEST_OS', 'rhel-8-4', 'RHEL version')
-        stringParam('ARCH', 'x86_64', 'CPU architecture')
     }
     properties {
         pipelineTriggers {
@@ -32,7 +30,7 @@ pipelineJob("$folderName/$jobName") {
                                     overrides {
                                         topic("Consumer.rh-jenkins-ci-plugin.${UUID.randomUUID().toString()}.VirtualTopic.eng.pungi.status-change")
                                     }
-                                    selector("status = 'FINISHED' AND compose_type = 'nightly' AND release_short = 'RHEL' AND release_version LIKE '8.4%' AND location NOT LIKE '%/updates/%'")
+                                    selector("status = 'FINISHED' AND compose_type = 'nightly' AND release_short = 'RHEL' AND (release_version LIKE '8.3%' OR release_version LIKE '8.4%' OR release_version LIKE '8.5%')")
                                 }
                             }
                         }
@@ -52,7 +50,7 @@ pipelineJob("$folderName/$jobName") {
                 }
             }
             lightweight(true)
-            scriptPath('Jenkinsfile.bare')
+            scriptPath('kitebot/Jenkinsfile')
         }
     }
 }
