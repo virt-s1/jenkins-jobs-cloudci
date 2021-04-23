209d936f50ed6740bad6ea3fe6c753f8646daf1b
Xiaofeng Wang <henrywangxf@me.com>
2021-04-23 22:28:44 +0800

    stage: Add github organization job

---
+++ jobs-stage/job_github_virt_s1_organization.groovy
@@ -0,0 +1,39 @@
+organizationFolder('Virt-S1') {
+    description('Github Organization Virt-S1')
+    displayName('Virt-S1')
+    organizations {
+        github {
+            apiUri('https://api.github.com')
+            credentialsId('github-cloudkitebot-credential')
+            repoOwner('virt-s1')
+            traits {
+                gitHubBranchDiscovery {
+                    strategyId(1)
+                }
+                gitHubPullRequestDiscovery {
+                    strategyId(2)
+                }
+                notificationContextTrait {
+                    typeSuffix(true)
+                    contextLabel('kitebot')
+                }
+            }
+        }
+    }
+    projectFactories {
+        workflowMultiBranchProjectFactory {
+            scriptPath('kitebot/Jenkinsfile')
+        }
+    }
+    triggers {
+        periodicFolderTrigger {
+            interval('1440')
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            daysToKeep(2)
+            numToKeep(5)
+        }
+    }
+}
