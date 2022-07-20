3c78c17d16b7237c59a36d1bd6dfb98e9d985dde
Xiaofeng Wang <henrywangxf@me.com>
2022-07-20 09:07:37 +0800

    jobs-stage: Add rhel for edge package jobs

---
+++ jobs-stage/job_rhel_edge_packages.groovy
@@ -0,0 +1,68 @@
+// Folder name and description
+String folderName = 'rhel-edge-packages'
+String folderDescription = 'Downstream rhel-edge package CI'
+// Repository URL
+String repo = 'https://github.com/virt-s1/rhel-edge.git'
+// Packages list
+List packages = ['greenboot', 'fido-device-onboard', 'rust-coreos-installer', 'rpm-ostree', 'ostree']
+// Distro list
+List distros = ['el8', 'el9']
+
+folder(folderName) {
+    description(folderDescription)
+}
+
+packages.each { pkg->
+    distros.each { dist->
+        String expected = '.*/' + pkg + '.*' + '\\.' + dist + '\\.' + '.*'
+        pipelineJob("$folderName/$pkg-$dist") {
+            logRotator(-1, 50, -1, -1)
+            parameters {
+                stringParam('CI_MESSAGE', '{}', 'Red Hat UMB Message Body')
+                stringParam('TEST_OS', "${dist}", 'RHEL release version')
+            }
+            properties {
+                pipelineTriggers {
+                    triggers {
+                        ciBuildTrigger {
+                            noSquash(true)
+                            providers {
+                                providerDataEnvelope{
+                                    providerData {
+                                        activeMQSubscriber {
+                                            name("Red Hat UMB")
+                                            overrides {
+                                                topic("Consumer.edge-qe-jenkins.${UUID.randomUUID().toString()}.VirtualTopic.eng.brew.task.closed")
+                                            }
+                                            selector("method = 'buildArch' AND new = 'CLOSED'")
+                                            checks {
+                                                msgCheck {
+                                                    field('$.info.request[0]')
+                                                    expectedValue(expected)
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            definition {
+                cpsScm {
+                    scm {
+                        git {
+                            remote {
+                                url(repo)
+                            }
+                            branch('*/master')
+                        }
+                    }
+                    lightweight(true)
+                    scriptPath("packages/Jenkinsfile.$pkg")
+                }
+            }
+        }
+    }
+}
